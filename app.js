379a4dcfa6d84f56434f7f3e07e802c210f7c52e
stylotylo <tylerlovell@gmail.com>
2022-05-08 12:14:47 -0400

    Implemented Seed data for building out App, started on CRUD (Create and Read implemented)

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
 const express = require(`express`) 
-const app = express();
 const path = require(`path`);
+const mongoose = require (`mongoose`);
+const Campground = require(`./models/campground`)
+
+mongoose.connect(`mongodb://localhost:27017/yelp-camp`, {
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
 
 app.set(`view engine`, `ejs`);
 app.set(`views`, path.join(__dirname,`views`))
 
+app.use(express.urlencoded({ extended: true}))
+
 app.get(`/`, (req,res) => {
     res.render(`home`)
 })
+
+app.get(`/campgrounds`, async (req,res) => {
+    const campgrounds = await Campground.find({});
+    res.render(`campgrounds/index`, {campgrounds});
+})
+
+app.get(`/campgrounds/new`, (req,res) => {
+    res.render(`campgrounds/new`);
+})
+
+app.post(`/campgrounds`, async(req,res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`);
+})
+
+app.get(`/campgrounds/:id`, async (req,res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render(`campgrounds/show`, {campground});
+})
+
+//Does this break anything when commented out?
+// app.get(`/makecampground`, async (req,res) => {
+//     const camp = new Campground({
+//         title: `New Campground!`,
+//         price: `Lots of Money`,
+//         description: `Cool and new campground`
+//     })
+//     await camp.save();
+//     res.send(camp);
+// })
 
 app.listen(3000, () => {
     console.log(`Serving on port 3000`)
