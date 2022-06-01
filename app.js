5956a75c901d5c2e73a0b9aad8b94b86515d2cc8
stylotylo <tylerlovell@gmail.com>
2022-06-01 08:24:09 -0400

    Added Reviews section with basic text field and rating (1-5). Ability to create and delete. Deleting a campground is not properly deleting associated reviews, need to debug this.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const ExpressError = require(`./utils/ExpressError`);
 const methodoverride = require (`method-override`);
 const Campground = require(`./models/campground`);
 const res = require("express/lib/response");
-const { campgroundSchema } = require(`./schemas`)
+const { campgroundSchema, reviewSchema } = require(`./schemas`)
+const Review = require(`./models/review`);
 
 mongoose.connect(`mongodb://localhost:27017/yelp-camp`, {
     useUnifiedTopology: true
@@ -31,6 +32,18 @@ app.use(methodoverride(`_method`));
 const validateCampground = (req,res,next) => {
     
     const { error } = campgroundSchema.validate(req.body)
+
+    if(error){
+        const msg = error.details.map(el => el.message).join(`,`)
+        throw new ExpressError(msg, 400)
+    }
+    else{
+        next();
+    }
+}
+
+const validateReview = (req,res,next) => {
+    const { error } = reviewSchema.valid(req.body);
 
     if(error){
         const msg = error.details.map(el => el.message).join(`,`)
@@ -65,7 +78,7 @@ app.post(`/campgrounds`, validateCampground, catchAsync( async(req,res,next) => 
 }))
 
 app.get(`/campgrounds/:id`, catchAsync ( async (req,res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate(`reviews`);
     res.render(`campgrounds/show`, {campground});
 }))
 
@@ -80,11 +93,27 @@ app.put(`/campgrounds/:id`, validateCampground, catchAsync (async(req,res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-app.delete(`/campgrounds/:id`, catchAsync (async(req,res) => {
+app.delete(`/campgrounds/:id`, validateReview, catchAsync (async(req,res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect(`/campgrounds`);
 
+}))
+
+app.post(`/campgrounds/:id/reviews`, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete(`/campgrounds/:id/reviews/:reviewId`, catchAsync(async(req,res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`)
 }))
 
 app.all(`*`, (req,res,next) => {
@@ -94,7 +123,6 @@ app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = `Oh no, default error message something went wrong and we dont have the specifics`
     res.status(statusCode).render(`error`, { err })
-    res.send(`Whoa, something went wrong!`)
 })
 
 app.listen(3000, () => {
