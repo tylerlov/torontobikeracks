4790c5dd44b9673130b0735e272a5c816ac266d5
stylotylo <tylerlovell@gmail.com>
2022-06-17 19:17:17 -0400

    ready to try deploying

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const flash = require(`connect-flash`)
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
+const mongoSanitize = require('express-mongo-sanitize')
+// const helmet = require('helmet')
 
+const MongoStore = require('connect-mongo');
 
 const res = require("express/lib/response");
 
@@ -21,7 +24,11 @@ const campgroundRoutes = require(`./routes/campgrounds`);
 const reviewRoutes = require(`./routes/reviews`);
 const userRoutes = require('./routes/users')
 
-mongoose.connect(`mongodb://localhost:27017/yelp-camp`, {
+const dbUrl = process.env.DB_URL || `mongodb://localhost:27017/yelp-camp`
+
+//dbUrl for when we deploy
+//`mongodb://localhost:27017/yelp-camp` for local
+mongoose.connect(dbUrl, {
     useUnifiedTopology: true
 })
 
@@ -40,14 +47,78 @@ app.set(`views`, path.join(__dirname,`views`))
 app.use(express.static(path.join(__dirname, `public`)));
 app.use(express.urlencoded({ extended: true}))
 app.use(methodoverride(`_method`));
+app.use(mongoSanitize());
+// app.use(helmet({
+//     contentSecurityPolicy: false,
+//   }))
 
+// const scriptSrcUrls = [
+//     "https://stackpath.bootstrapcdn.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://api.mapbox.com/",
+//     "https://kit.fontawesome.com/",
+//     "https://cdnjs.cloudflare.com/",
+//     "https://cdn.jsdelivr.net",
+// ];
+// //This is the array that needs added to
+// const styleSrcUrls = [
+//     "https://kit-free.fontawesome.com/",
+//     "https://api.mapbox.com/",
+//     "https://api.tiles.mapbox.com/",
+//     "https://fonts.googleapis.com/",
+//     "https://use.fontawesome.com/",
+//     "https://cdn.jsdelivr.net",
+// ];
+// const connectSrcUrls = [
+//     "https://api.mapbox.com/",
+//     "https://a.tiles.mapbox.com/",
+//     "https://b.tiles.mapbox.com/",
+//     "https://events.mapbox.com/",
+// ];
+// const fontSrcUrls = [];
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives: {
+//             defaultSrc: [],
+//             connectSrc: ["'self'", ...connectSrcUrls],
+//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//             workerSrc: ["'self'", "blob:"],
+//             objectSrc: [],
+//             imgSrc: [
+//                 "'self'",
+//                 "blob:",
+//                 "data:",
+//                 "https://res.cloudinary.com/ds2hafwml", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+//             ],
+//             fontSrc: ["'self'", ...fontSrcUrls],
+//         },
+//     })
+// );
+
+const secret = process.env.SECRET || `thiscouldbebetter`
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret
+    }
+});
+
+store.on("error", function (e) {
+    console.log("Session Store Error!", e)
+})
 
 const sessionConfig = {
-    secret: `thiscouldbebetter`,
+    store,
+    name: 'tylerscoolguysitethatgetshimajob',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        //secure: true,
         // This is in milliseconds 
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,11 +1,20 @@
-const campground = require("../../models/campground");
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10 // starting zoom
+});
 
-    mapboxgl.accessToken = mapToken;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: campground.geometry.coordinates,
-        zoom: 8 // starting zoom
-    });
+map.addControl(new mapboxgl.NavigationControl());
 
-    new mapboxgl.Marker().setLngLat(75,40).addTo(map)
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
+    )
+    .addTo(map)
+
