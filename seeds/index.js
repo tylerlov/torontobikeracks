22a10b2e7f9b41c41de1dd54ee247083effe3d4c
stylotylo <tylerlovell@gmail.com>
2022-06-23 14:40:09 -0400

    Revert "Adjusted seeding file to upload to my mongo atlas cluster."
    This reverts commit 8322f317104ca7a142f0912c768d46eab4f50f26.

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,9 @@
 const mongoose = require (`mongoose`);
 const Bikerack = require(`../models/bikerack`)
 const bikedata = require('./bikedata')
-const user = require(`../models/user`)
 
 
-mongoose.connect(`mongodb+srv://siteAdmin:eWl9eg0Mygtz6Uit@cluster0.jztlk.mongodb.net/?retryWrites=true&w=majority`, {
+mongoose.connect(`mongodb://localhost:27017/torontobikeracks`, {
     useUnifiedTopology: true
 })
 
@@ -19,16 +18,9 @@ db.once("open", () => {
 
 const seedDB = async () => {
     await Bikerack.deleteMany({});
-    await user.deleteMany({});
-    const defaultUser = new user({
-      username: 'Tyler',
-      password: 'rezinfinite',
-      email: 'tylerlovell@gmail.com',
-  })
-  await defaultUser.save();
     for(let i = 0; i < 117; i++){
             const bikes = new Bikerack({
-                author: defaultUser._id,
+                author: '62b1a3b16b562b4cf76cd71d',
                 location: `${bikedata[i].properties.ADDRESS_FULL}, ${bikedata[i].properties.CITY}`,
                 title: `${bikedata[i].properties.PARKING_TYPE}`,
                 description: `Installed in ${bikedata[i].properties.YEAR_INSTALLED}, this ${bikedata[i].properties.PARKING_TYPE} can hold ${bikedata[i].properties.BICYCLE_CAPACITY} bikes and belongs to Ward ${bikedata[i].properties.WARD}.`,
@@ -51,8 +43,6 @@ const seedDB = async () => {
             })
         await bikes.save();
     }
-    
-
 }
 
 seedDB().then(() => {
